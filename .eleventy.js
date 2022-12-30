205789f463027d1dfac0b09f944cca75aa7aab8b
lolbest1800 <109381827+lolbest1800@users.noreply.github.com>
2022-12-30 19:54:59 +0000

    hot fix

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,11 @@
 
 module.exports = function (eleventyConfig) {
     
-    eleventyConfig.addPassthroughCopy("./src/style.css")
-    eleventyConfig.addPassthroughCopy("./src/static/")
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/static/");
 
-    eleventyConfig.addPassthroughCopy("./src/admin/")
-    eleventyConfig.addPassthroughCopy("./src/uploaded-images/")
+    eleventyConfig.addPassthroughCopy("./src/admin/");
+    eleventyConfig.addPassthroughCopy("./src/uploaded-images/");
     
     
     return {
